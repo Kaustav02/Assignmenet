d8b7914ef026bef29c46974cda40b034d9f2922f
kaustav <kaustav.mondal2003@gmail.com>
2023-04-16 20:32:44 +0530

    3rd

--- pop.cpp
+++ pop.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// Approach for Q1
+// I have used two pointer approach
+//  Old Queue		[A B C D E F A* B C D A B C A B A]
+//    New Queue	     [B C E F B C B C B]
+
+// I have set pointer i to Old Queue and a j pointer at New Queue and I am running the loop until i is not equal to the given currentIndex-1
+// If the size of the New Queue is 1 then we simply return 1 as our answer.
+// In the loop, if the i-th element is not equal to the j-th element of the Old Queue I am simply doing i++ 
+// If elements are equal I am doing i++ and j++;
+// In that way, I have obtained the value of the new index which is stored in j.
+
+// Time Complexity is O(n) (if the value of the index is equal to size)
+
 int main()
 {
 
